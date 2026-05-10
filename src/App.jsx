a618872e0d16dc968706839883b1962df3091f59
Sherzod Ataxonov <ataxonovsherzod77@gmail.com>
2026-05-10 22:53:41 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Admin from './admin/Admin'
+import Oshpaz from './oshpaz/Oshpaz'
+import Kasser from './Kasser/Kasser'
+import Afitsant from './afitsant/Afitsant'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1> <Admin/></h1>
+    <h1><Oshpaz/></h1>
+    <h1><Kasser/></h1>
+    <h1><Afitsant/></h1>
+    </>
+  )
+}
+
+export default App
